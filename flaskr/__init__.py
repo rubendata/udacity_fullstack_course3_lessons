c63e39477b3070eb0cced11fb47df2a607cf107f
rubendata <info@rubensimon.de>
2021-01-11 20:50:30 +0100

    added initial flask app

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,1 +1,13 @@
-from flask import 
+from flask import Flask, jsonify
+
+def create_app(test_config=None):
+    app = Flask(__name__)
+    @app.route('/')
+    def hello():
+        return jsonify({'message': 'hello world'})
+
+    @app.route('/smile')
+    def smile():
+        return ";)"
+    return app
+    
